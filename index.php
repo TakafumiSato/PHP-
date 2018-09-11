a52905e946bdf5dd5b40562dd9dc4430bd1feac5
TakafumiSato <sato@augusta-inc.co.jp>
2018-09-11 11:29:27 +0900

    PHP入門

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+/*
+変数：データにつけるラベル
+
+データ型：
+- 文字列 string
+- 数値 integer float
+- 論理値 boolean true/false
+- 配列
+- オブジェクト
+- null
+*/
+
+$msg = "Hello from the TOP!!";
+echo $msg;
+var_dump($msg);
---
+++ index2.php
@@ -0,0 +1,11 @@
+<?php
+
+// 定数：変更されない値につけるラベル
+
+define( MY_EMAIL, "sato@augusta-inc.co,jp" );
+
+echo MY_EMAIL;
+
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);
