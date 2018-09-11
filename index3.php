0ce78d19b0c6d1bd4aa77dc27f6571c4d4cb761a
TakafumiSato <sato@augusta-inc.co.jp>
2018-09-11 13:38:33 +0900

    PHP入門Chapter7まで

---
+++ index3.php
@@ -0,0 +1,33 @@
+<?php
+
+// ”’lŒ^‚Ì‰‰ŽZ
+
+// + - * / % **(PHP5.6~)
+
+// $x = 10 % 3; // 1
+// $y = 30.2 / 4; // 7.55
+// var_dump($x);
+// var_dump($y);
+
+// ’P€‰‰ŽZŽq
+// $z = 5;
+// $z++; // 6
+// var_dump($z);
+// $z--; // 5
+// var_dump($z);
+
+// ‘ã“ü‚ð”º‚¤‰‰ŽZŽq
+// $x = 5;
+// //$x = $x + 2; // 10
+// $x *= $x; // 25
+// var_dump($x);
+
+
+// $name = "sato";
+// var_dump("Hello $name\nHello again!");
+// var_dump("Hello {$name}\nHello again!");
+// var_dump("Hello ${name}\nHello again!");
+// var_dump('Hello $name\nHello again!');
+
+$s = "Hello" . "World";
+var_dump($s);
