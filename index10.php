ace76dd407cbc6a2aa4ea4fa6988575beaa46a23
TakafumiSato <sato@augusta-inc.co.jp>
2018-09-12 10:51:10 +0900

    認証ダイアログ作成

---
+++ index10.php
@@ -0,0 +1,39 @@
+
+// 認証ダイアログ
+
+<?php
+$user = 'admin';
+$password = 'passs';
+
+// Authorizationヘッダの有無を確認
+// 初めてのアクセスの場合、ついていない
+if (!isset($_SERVER['PHP_AUTH_USER'])){
+    // 表示
+    header('WWW-Authenticate: Basic realm="Private Page"');
+    header('HTTP/1.0 401 Unauthorized');
+
+    // キャンセルが押された
+    die('このページを見るにはログインが必要です');
+}else{
+    // 入力が一致しているか
+    if ($_SERVER['PHP_AUTH_USER'] != $user
+        || $_SERVER['PHP_AUTH_PW'] != $password){
+
+        header('WWW-Authenticate: Basic realm="Private Page"');
+        header('HTTP/1.0 401 Unauthorized');
+
+        // キャンセルが押された場合
+        die('このページを見るにはログインが必要です');
+    }
+}
+
+?>
+
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<p>ようこそ、会員向けページへ</p>
+
+</body>
+</html>
